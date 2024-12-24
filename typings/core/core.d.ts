c6b36914ebeaeae183a7147054bf5489537c580c
eveningwater <854806732@qq.com>
2024-12-24 07:01:35 +0000

    feat: 修改了打包

--- typings/core/core.d.ts
+++ typings/core/core.d.ts
@@ -1,6 +1,8 @@
 import { WatchOptions } from '@vue/reactivity';
 import { StoreType } from './enum';
+import { parseStr } from './utils';
 export interface StoreOptions extends WatchOptions {
     storage?: StoreType;
 }
 export declare function useStorage<T>(key: string, initialValue: T, options?: StoreOptions): [T] extends [import("@vue/reactivity").Ref<any, any>] ? import("@vue/shared").IfAny<T, import("@vue/reactivity").Ref<T, T>, T> : import("@vue/reactivity").Ref<import("@vue/reactivity").UnwrapRef<T>, T | import("@vue/reactivity").UnwrapRef<T>>;
+export { parseStr };
